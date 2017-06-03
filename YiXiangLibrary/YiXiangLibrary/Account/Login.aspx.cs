aa4683a8d16cae17492cd6c6f0f966fc51aad7c2
OnlyShe <374710624@qq.com>
2017-06-04 00:00:28 +0800

    医生顾客注册登陆

--- YiXiangLibrary/YiXiangLibrary/Account/Login.aspx.cs
+++ YiXiangLibrary/YiXiangLibrary/Account/Login.aspx.cs
@@ -4,14 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace YiXiangLibrary.Account
 {
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        { }
+
+        protected void LoginButton_Click1(object sender, EventArgs e)
         {
-            RegisterHyperLink.NavigateUrl = "Register.aspx?ReturnUrl=" + HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            SqlConnection conn = new SqlConnection(DbConnection.connection());
+            conn.Open();
+            string str = "select * from users where u_id='" + UserName.Text.ToString().Trim() + "' and pass='" + Password.Text.ToString().Trim() + "'";
+            SqlCommand cmd = new SqlCommand(str, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                Response.Redirect("..//Default.aspx");
+            }
+            else
+            {
+                Label1.Text = "账号或密码错误，请重新输入！";
+                UserName.Text = "";
+                Password.Text = "";
+            }
         }
     }
 }
---
+++ YiXiangLibrary/YiXiangLibrary/DbConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YiXiangLibrary
+{
+    public class DbConnection
+    {
+        public static string connection()
+        {
+            return "server=.;database=yixiang;Integrated Security=SSPI";
+        }
+    }
+}
