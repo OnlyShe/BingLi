6f315098aa1acb87437f1157c6df2363ed10f602
OnlyShe <374710624@qq.com>
2017-06-04 09:42:30 +0800

    显示病例搜索结果

---
+++ YiXiangLibrary/YiXiangLibrary/BingliClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YiXiangLibrary
+{
+    public class BingliClass
+    {
+        private string disease_name;
+        private string desc;
+        private string people;
+
+        public void SetBingli(string name, string desc, string people)
+        {
+            this.disease_name = name;
+            this.desc = desc;
+            this.people = people;
+        }
+    }
+}
