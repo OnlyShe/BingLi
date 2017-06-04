f5243086e072467ca9de03fafbbb8c3bff30cbf6
OnlyShe <374710624@qq.com>
2017-06-04 16:38:39 +0800

    用户病例查询

---
+++ YiXiangLibrary/YiXiangLibrary/Bingli_user.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page MasterPageFile="~/Bingli_Header.master"  Language="C#" AutoEventWireup="true" CodeBehind="Bingli_user.aspx.cs" Inherits="YiXiangLibrary.Bingli_user" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+<div style="width:100%;">
+    <div class="bingli_div" style="width:68%;display:inline-block;border-right:1px solid gray;">
+    <%  for(int i=0;i<count;i++)
+        { %>
+   
+        <a href="#">
+        <div class="bingli_juti">
+        <h2>[好转]</h2>
+        <h3><%Response.Write(data1[i].ToString());%></h3>
+        <br />
+        <h4><%Response.Write(data2[i].ToString()); %></h4>
+        <br />
+        <br />
+        <h4>[发布人]</h4>
+        <h4><%Response.Write(data3[i].ToString()); %></h4>
+        </div>
+        </a>
+    <%} %>
+    </div>
+    <div class="bingli_div" style="width:28%;display:block;float:right;">
+        <div class="bingli_warm">
+            <h3>[温馨提示]</h3>
+            <br />
+            <h4>
+            但愿身长健,浮世拚悠悠。
+            <br />——曾觌
+            </h4>
+            <br />
+            <h4>
+            只祈彼此身长健,同处何曾有别离。
+            </br/>——魏了翁 
+            </h4>
+            <br />
+            <h4>
+            浮云富贵非公愿,只愿公身健。
+            <br />——魏了翁
+            </h4>
+        </div>
+    </div>
+</div>
+</asp:Content>
+
---
+++ YiXiangLibrary/YiXiangLibrary/Bingli_user.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace YiXiangLibrary
+{
+    public partial class Bingli_user : System.Web.UI.Page
+    {
+        public ArrayList data1 = new ArrayList();
+        public ArrayList data2 = new ArrayList();
+        public ArrayList data3 = new ArrayList();
+        public int count = 0;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            data1.Clear();
+            data2.Clear();
+            data3.Clear();
+            count = 0;
+            BingliClass bl = new BingliClass();
+            string name = "", des = "", people = "";
+            SqlConnection conn = new SqlConnection(DbConnection.connection());
+            string str = "select doctors.d_name,dis_name,explain.e_express from doctors,explain,disease where doctors.d_id=explain.d_id and explain.dis_id=disease.dis_id";
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(str, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                name = reader["dis_name"].ToString().Trim();
+                des = reader["d_name"].ToString().Trim();
+                people = reader["e_express"].ToString().Trim();
+                bl.SetBingli(name, des, people);
+                data1.Add(name);
+                data2.Add(des);
+                data3.Add(people);
+                bl.SetBingli(null, null, null);
+                count++;
+            }
+            reader.Close();
+
+        }
+    }
+}
